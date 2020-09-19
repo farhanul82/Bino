339a1d793e22c76c637496a67492ece5ea225c1a
farhanul82 <kazi82@student.sust.edu>
2020-09-19 16:39:35 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+mybutton = document.getElementById("myBtn");
+
+        window.onscroll = function () { scrollFunction() };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+
+
+        window.addEventListener("scroll", function () {
+            let header = document.querySelector(".header-section");
+            header.classList.toggle("sticky", window.scrollY > 0);
+        })
